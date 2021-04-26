4cf416540d230e8e8ff6b9038089380e2f1da828
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-04-26 18:03:47 +0530

    Completion of the banner CTA Section on the page

--- pages/product-tracker/index.tsx
+++ pages/product-tracker/index.tsx
@@ -2,6 +2,7 @@
 import HeroBox from '../../containers/ProductTracker/HeroBox';
 import InfoSection from '../../containers/ProductTracker/InfoSection';
 import BenefitsSection from '../../containers/ProductTracker/BenefitsSection';
+import BannerCTASection from '../../containers/ProductTracker/BannerCTASection';
 import PlansSection from '../../containers/ProductTracker/PlansSection';
 import OtherInfoSection from '../../containers/ProductTracker/OtherInfoSection';
 import FAQSection from '../../containers/ProductTracker/FAQSection';
@@ -17,6 +18,7 @@ const ProductTracker = () => {
 			<section className={styles.banner} />
 			<InfoSection />
 			<BenefitsSection />
+			<BannerCTASection />
 			<PlansSection />
 			<OtherInfoSection />
 			<FAQSection />
