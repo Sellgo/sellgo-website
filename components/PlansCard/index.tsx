90d38e9c16293077ec329b0a26876c3ae9bcd257
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-05-07 16:58:33 +0530

    Addition of the popular features toggle functionality for the plans section on the product pages

--- components/PlansCard/index.tsx
+++ components/PlansCard/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 
 /* Styling */
 import styles from './index.module.scss';
@@ -6,27 +7,46 @@ import styles from './index.module.scss';
 interface Props {
 	name: string;
 	startingPrice: number;
+	featureList: string[];
 }
 
 const PlansCard: React.FC<Props> = (props) => {
-	const { name, startingPrice } = props;
+	const { name, startingPrice, featureList } = props;
+
+	const [isExpanded, setIsExpanded] = useState(false);
 
 	return (
 		<div className={styles.plansCard}>
-			<h3>{name}</h3>
+			<div className={styles.plansCard__Content}>
+				<h3>{name}</h3>
 
-			{startingPrice === 0 ? (
-				<>
-					<small>100% free</small>
-					<p>No credit card required</p>
-				</>
-			) : (
-				<p>{`Starting at $${startingPrice}/month`}</p>
-			)}
+				{startingPrice === 0 ? (
+					<>
+						<small>100% free</small>
+						<p>No credit card required</p>
+					</>
+				) : (
+					<p>{`Starting at $${startingPrice}/month`}</p>
+				)}
+			</div>
 
-			<button>
-				Popular Features <span>&#43;</span>
+			<button onClick={() => setIsExpanded(!isExpanded)}>
+				Popular Features
+				{isExpanded ? <span>&#8722; </span> : <span>&#43;</span>}
 			</button>
+
+			{isExpanded && (
+				<ul className={styles.popularFeaturesList}>
+					{featureList.map((feature: string) => {
+						return (
+							<li key={uuid()}>
+								<span>&#x2713; </span>
+								{feature}
+							</li>
+						);
+					})}
+				</ul>
+			)}
 		</div>
 	);
 };
