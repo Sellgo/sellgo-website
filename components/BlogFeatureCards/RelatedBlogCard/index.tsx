90480e9dea7779fd58b27b148d5bfd36addfa8cd
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-05-07 20:09:41 +0530

    Addition of navigation for blogs page

--- components/BlogFeatureCards/RelatedBlogCard/index.tsx
+++ components/BlogFeatureCards/RelatedBlogCard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 /* Styling */
 import styles from './index.module.scss';
@@ -12,20 +13,24 @@ interface Props {}
 const RelatedBlogCard: React.FC<Props> = () => {
 	return (
 		<article className={styles.relatedBlogCard}>
-			<div className={styles.bgImage}>
-				<Image
-					loader={imageLoaderForBlogs}
-					src="https://blog.hubspot.com/hubfs/excel-marketing-templates.jpg"
-					alt="Alt Text"
-					layout="fill"
-					objectFit="cover"
-				/>
-			</div>
+			<Link href="/" passHref>
+				<a>
+					<div className={styles.bgImage}>
+						<Image
+							loader={imageLoaderForBlogs}
+							src="https://blog.hubspot.com/hubfs/excel-marketing-templates.jpg"
+							alt="Alt Text"
+							layout="fill"
+							objectFit="cover"
+						/>
+					</div>
 
-			<div className={styles.blogText}>
-				<h2>Problems with Amzon Seller</h2>
-				<p>marketing | 15 mins read</p>
-			</div>
+					<div className={styles.blogText}>
+						<h2>Problems with Amzon Seller</h2>
+						<p>marketing | 15 mins read</p>
+					</div>
+				</a>
+			</Link>
 		</article>
 	);
 };
