e9c5011708d8047de52bedf4935795f40150de87
rwprasojo <richard@sellgo.com>
2021-05-12 12:16:51 -0700

    RP_18.3_2B

--- containers/FreeTrialPanel/FeaturesSection/data.ts
+++ containers/FreeTrialPanel/FeaturesSection/data.ts
@@ -7,7 +7,13 @@ export const featuresData = [
 			'Bulk Export',
 			'Connect to Product Tracker'
 		],
-		popularFeaturesList: ['Feature 1', 'Feature 2']
+		popularFeaturesList: [
+			'Amazon Sales Estimator 3 estimates daily',
+			'True Sales Estimates',
+			'Leaf Sourcing',
+			'Bulk Export to .CSV or .XLSX',
+			'Streamline Product Ideas to Daily Tracking'
+		]
 	},
 	{
 		title: 'Product Research',
@@ -18,17 +24,28 @@ export const featuresData = [
 			'Variation Analysis',
 			'Multipack Analysis'
 		],
-		popularFeaturesList: ['Feature 1', 'Feature 2']
+		popularFeaturesList: [
+			'Search Management with bulk export',
+			'Profit Finder with 30 products bulk processing daily',
+			'Data Input as UPC, EAN, ASIN, ISBN',
+			'Upload Error Analysis',
+			'Data Buster with 3 products processing daily',
+			'Smart Preset',
+			'Variation Analysis',
+			'Multipack Analysis',
+			'Product Tracker with 3 products tracking',
+			'Data Visualization',
+			'True Sales Estimates',
+			'Historical Product Tracking 1 Month'
+		]
 	},
 	{
 		title: 'Seller Research',
-		featuresList: [
+		featuresList: ['Super Targeting', 'Seller Database', 'Seller Finder'],
+		popularFeaturesList: [
 			'Super Targeting',
 			'Seller Database',
-			'Seller Finder',
-			'Brand Finder',
-			'Check Seller Inventory'
-		],
-		popularFeaturesList: ['Feature 1', 'Feature 2']
+			'Seller Finder 2 sellers daily'
+		]
 	}
 ];
