a5e2779293ab59f6c99a91c380abecc806b3208d
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-05-21 00:08:41 +0530

    Connect missing SEO on pages

---
+++ data/SEO/aboutPage.ts
@@ -0,0 +1,15 @@
+export const seoData = {
+	title: `Sellgo - About Us`,
+	description: `Sellgo was founded in 2018 and is headquarted in Portland, Oregon. 
+	Sellgo is an Amazon FBA product research tool to empower sellers to grow their Amazon businesses. 
+	Expand your business with our FBA tools and resources.`,
+	imageUrl: ``,
+	keywords: [
+		'sellgo',
+		'what is sellgo',
+		'sellgo company',
+		'sellgo headquarters',
+		'sellgo portland'
+	],
+	slug: '/about-us'
+};
---
+++ data/SEO/demo.ts
@@ -0,0 +1,18 @@
+export const seoData = {
+	title: `Sellgo - Request a Demo`,
+	description: `Connect with our Sales Team and demo our tools and services. 
+	Get to know our platform and how you can grow your 
+	Amazon business and increase your profits with Sellgo! `,
+	imageUrl: ``,
+	keywords: [
+		'sellgo',
+		'sellgo blog',
+		'amazon blog',
+		'blog amazon fba',
+		'amazon fba',
+		'amazon wholesale',
+		'amazon news',
+		'amazon marketplace'
+	],
+	slug: '/demo'
+};
