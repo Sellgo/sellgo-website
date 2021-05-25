b2810c10550b6f8d07bae9c667de6dc605bb732c
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-05-25 23:34:42 +0530

    Addition of the query cards for the contact us page

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Link from 'next/link';
 
 /* Styling */
-// import styles from './index.module.scss';
+import styles from './index.module.scss';
 
 /* Containers */
 import HeroBox from '../../containers/Contact/HeroBox';
@@ -12,6 +13,51 @@ const ContactUsPage: React.FC<Props> = () => {
 	return (
 		<main>
 			<HeroBox />
+
+			<section className={`page-container ${styles.queryCardsWrapper}`}>
+				<div className={styles.queryCard}>
+					<h3>Media Inquiry</h3>
+					<p>Please Contact:</p>
+					<div className={styles.queryCard__links}>
+						<a href="mailto:press@sellgo.com" className="anchor">
+							press@sellgo.com
+						</a>
+					</div>
+				</div>
+
+				<div className={styles.queryCard}>
+					<h3>Affiliate Partner</h3>
+					<p>Please Contact:</p>
+
+					<div className={styles.queryCard__links}>
+						<a href="mailto:affiliate@sellgo.com" className="anchor">
+							affiliate@sellgo.com
+						</a>
+						<p>
+							or visit{' '}
+							<Link href="/affiliate-program">
+								<a className="anchor">Affiliate Program</a>
+							</Link>
+						</p>
+					</div>
+				</div>
+
+				<div className={styles.queryCard}>
+					<h3>Questions</h3>
+					<p>Please Contact:</p>
+					<div className={styles.queryCard__links}>
+						<a href="mailto:support@sellgo.com" className="anchor">
+							support@sellgo.com
+						</a>
+						<p>
+							or visit{' '}
+							<Link href="/contact">
+								<a className="anchor">Contact Us</a>
+							</Link>
+						</p>
+					</div>
+				</div>
+			</section>
 		</main>
 	);
 };
