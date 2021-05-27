f02763f15827bea0957df4c32374eabb8b4127fb
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-05-28 02:29:39 +0530

    Improvements in head component and BUGFR-1353 and BUGFR-1354

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,13 +16,13 @@ class MyDocument extends Document {
 	render() {
 		return (
 			<Html>
-				<Head />
-
-				{/* Hotjar Integration */}
-				<script
-					defer
-					dangerouslySetInnerHTML={{
-						__html: `(function(h,o,t,j,a,r){
+				<Head>
+					<link rel="shortcut icon" href="/favicon.ico" />
+					{/* Hotjar Integration */}
+					<script
+						defer
+						dangerouslySetInnerHTML={{
+							__html: `(function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:1573858,hjsv:6};
         a=o.getElementsByTagName('head')[0];
@@ -30,26 +30,26 @@ class MyDocument extends Document {
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')`
-					}}
-				/>
+						}}
+					/>
 
-				{/* Intercom settings */}
-				<script
-					defer
-					dangerouslySetInnerHTML={{
-						__html: `  window.intercomSettings = {
+					{/* Intercom settings */}
+					<script
+						defer
+						dangerouslySetInnerHTML={{
+							__html: `  window.intercomSettings = {
     app_id: "ju2u3hej",
 		custom_launcher_selector:'#chat-link'
   };
 `
-					}}
-				/>
+						}}
+					/>
 
-				{/* Intercom main script */}
-				<script
-					defer
-					dangerouslySetInnerHTML={{
-						__html: `(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function")
+					{/* Intercom main script */}
+					<script
+						defer
+						dangerouslySetInnerHTML={{
+							__html: `(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function")
 						{ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function()
 							{i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function()
 							{var s=d.createElement('script');s.type='text/javascript';
@@ -57,8 +57,10 @@ class MyDocument extends Document {
 							var x=d.getElementsByTagName('script')[0];
 							x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}
 							else{w.addEventListener('load',l,false);}}})()`
-					}}
-				/>
+						}}
+					/>
+				</Head>
+
 				<body>
 					<Main />
 					<NextScript />
