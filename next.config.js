febd894cdd73006f284268795ee6d979d0aa3332
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-06-19 03:07:13 +0530

    Make permanent redirects from old blog to new blogs and docker file change

---
+++ next.config.js
@@ -0,0 +1,58 @@
+/* eslint-disable max-len */
+module.exports = {
+	async redirects() {
+		return [
+			{
+				source:
+					'/post/the-quick-amazon-sellers-guide-to-black-friday-and-cyber-monday',
+				destination: '/blogs/blog/guide-black-friday-and-cyber-monday-2020',
+				permanent: true
+			},
+			{
+				source:
+					'/post/a-beginner-s-guide-to-selling-wholesale-products-on-amazon-how-to-find-winning-products',
+				destination:
+					'/blogs/blog/guide-selling-wholesale-products-on-amazon-find-winning-products',
+				permanent: true
+			},
+			{
+				source: '/post/profit-calculator-estimate-amazon-product-margin',
+				destination:
+					'/blogs/blog/profit-calculator-how-to-estimate-amazon-product-margin',
+				permanent: true
+			},
+			{
+				source: '/post/super-tracking-develop-your-own-superpower',
+				destination:
+					'/blogs/blog/super-tracking-amazon-top-sellers-products-to-sell-wholesale-suppliers',
+				permanent: true
+			},
+			{
+				source:
+					'/post/a-guide-to-product-analysis-amazon-wholesale-and-product-sourcing',
+				destination:
+					'/blogs/blog/guide-product-analysis-the-secret-to-amazon-wholesale-and-product-sourcing',
+				permanent: true
+			},
+			{
+				source:
+					'/post/ultimate-guide-to-amazon-packaging-prepping-amazon-products-for-shipment',
+				destination:
+					'/blogs/blog/guide-amazon-packaging-prepping-amazon-products-for-shipment',
+				permanent: true
+			},
+			{
+				source:
+					'/post/4-best-amazon-reviewer-checkers-for-shoppers-and-amazon-businesses',
+				destination: '/blogs/blog/4-best-amazon-reviewer-checkers',
+				permanent: true
+			},
+			// slug match
+			{
+				source: '/post/:slug',
+				destination: '/blogs/blog/:slug',
+				permanent: true
+			}
+		];
+	}
+};
