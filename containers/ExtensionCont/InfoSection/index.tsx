36a36bc25b107c54c1040c3b6955f56be3d1043a
rwprasojo <richard@sellgo.com>
2021-07-12 14:05:59 -0700

    RP_28.1

--- containers/ExtensionCont/InfoSection/index.tsx
+++ containers/ExtensionCont/InfoSection/index.tsx
@@ -14,7 +14,7 @@ const InfoSection: React.FC<Props> = () => {
 				<div className={styles.infoSection__Text}>
 					<h2>What is the Sellgo Chrome Extension?</h2>
 					<p>
-						Sellg&apos;s Chrome Extension is an all-in-one Amazon seller
+						Sellgo&apos;s Chrome Extension is an all-in-one Amazon seller
 						toolbox. Access Sellgo&apos;s reliable FBA tools and real-time data
 						analytics to help you find replenishable products with the best
 						profits and return on investments directly within your browser.
