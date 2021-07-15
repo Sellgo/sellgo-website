88259a419277a0efdab31216955b99b8fc1dda59
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-07-16 03:57:55 +0530

    BUGFR-1650:Bring back the faqSection for the product pages

--- pages/profit-finder/index.tsx
+++ pages/profit-finder/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+import { GetStaticProps } from 'next';
 
 /* Containers */
 import HeroBox from '../../containers/ProfitFinderCont/HeroBox';
@@ -6,6 +8,7 @@ import InfoSection from '../../containers/ProfitFinderCont/InfoSection';
 import NewBenefitsSection from '../../containers/ProfitFinderCont/NewBenefitsSection';
 import CommonFeaturesSection from '../../containers/ProfitFinderCont/CommomFeaturesSection';
 import RecommendationSection from '../../containers/ProfitFinderCont/RecommendationSection';
+import FAQSection from '../../containers/ProfitFinderCont/FAQSection';
 
 /* Components */
 import SEOHead from '../../components/SEOHead';
@@ -16,9 +19,18 @@ import { seoData } from '../../data/SEO/profitFinder';
 /* Utils */
 import { generatePageURL } from '../../utils/SEO';
 
-interface Props {}
+/* Config */
+import AppConfig from '../../config';
 
-const ProfitFinderContPage: React.FC<Props> = () => {
+/* Types */
+import { FAQDetails } from '../../interfaces/FAQ';
+
+interface Props {
+	faqDetails: FAQDetails;
+}
+
+const ProfitFinderContPage: React.FC<Props> = (props) => {
+	const { faqDetails } = props;
 	return (
 		<>
 			<SEOHead
@@ -32,9 +44,21 @@ const ProfitFinderContPage: React.FC<Props> = () => {
 			<InfoSection />
 			<NewBenefitsSection />
 			<CommonFeaturesSection />
+			{faqDetails.data.length > 0 && <FAQSection faqData={faqDetails.data} />}
 			<RecommendationSection />
 		</>
 	);
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+	const response = await axios.get(`${AppConfig.FAQ_BUCKET}/profitFinder.json`);
+	const { data } = response;
+	return {
+		props: {
+			faqDetails: data
+		},
+		revalidate: 60 * 15 // 15 minutes
+	};
 };
 
 export default ProfitFinderContPage;
