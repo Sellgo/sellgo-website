457dd79484e50d2a726070a324009e398902edab
rwprasojo <richard@sellgo.com>
2021-07-16 15:27:18 -0700

    RP_28.5_2

--- data/SEO/sellerDatabase.ts
+++ data/SEO/sellerDatabase.ts
@@ -2,8 +2,17 @@ import { SEODetails } from '../../interfaces/SEO';
 
 export const seoData: SEODetails = {
 	title: `Sellgo - Amazon Seller Database:`,
-	description: ``,
+	description: `Access a database full of Amazon sellers 
+	and businesses selling profitable, high-demand products. 
+	Compare ASINs, brands, seller reviews ratings, and more.`,
 	imageUrl: ``,
-	keywords: [''],
+	keywords: [
+		'amazon network',
+		'amazon partner network',
+		'amazon online shopping network',
+		'amazon community',
+		'amazon best seller data',
+		'amazon marketplace seller data'
+	],
 	slug: '/seller-database'
 };
