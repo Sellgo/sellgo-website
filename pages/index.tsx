ca805ef992d80075b8f01dbe1e4ee2af64e35f09
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-07-21 21:33:54 +0530

    Move the imports to the correct location

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,9 +12,11 @@ import { GET_SHOW_CASE_BLOGS } from '../graphql/cms';
 
 /* Containers */
 import HeroBox from '../containers/HomePage/HeroBox';
+import InfoSection from '../containers/HomePage/InfoSection';
 import OpportunityBannerSection from '../containers/HomePage/OpportunityBannerSection';
 import GlobalSellersBannerSection from '../containers/HomePage/GlobalSellersBannerSection';
 import ProductsSection from '../containers/HomePage/ProductsSection';
+import StatisticsSection from '../containers/HomePage/StatisticsSection';
 import TestimonialsSection from '../containers/HomePage/TestimonialsSection';
 import RecentBlogsSection from '../containers/HomePage/RecentBlogsSection';
 import ClosingCTASection from '../containers/HomePage/ClosingCTASection';
@@ -30,8 +32,6 @@ import { generatePageURL } from '../utils/SEO';
 
 /* Types */
 import { ShowcaseBlogDetails } from '../interfaces/Blogs';
-import StatisticsSection from '../containers/HomePage/StatisticsSection';
-import InfoSection from '../containers/HomePage/InfoSection';
 
 interface Props {
 	homeBlogs: ShowcaseBlogDetails[];
