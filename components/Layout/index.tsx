01f638fdc207ff6626126bebcdc8489b2c476b91
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-07-30 23:15:03 +0530

    Addition of facebook pixel tracking

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 /* Analytics */
 import analytics from '../../analytics';
@@ -41,6 +42,28 @@ const Layout: React.FC<Props> = ({ children }) => {
 
 	return (
 		<>
+			<Head>
+				{/* Facebook Pixel Code */}
+				{isCookieConsentAccepted && (
+					<script
+						defer
+						// eslint-disable-next-line react/no-danger
+						dangerouslySetInnerHTML={{
+							__html: `!function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', ${String(process.env.NEXT_PUBLIC_FB_PIXEL_ID)});
+                fbq('track', 'PageView');`
+						}}
+					/>
+				)}
+			</Head>
+
 			{!hideNavigationOnRoutes.includes(asPath) && <Navbar />}
 			<MobileNavBar />
 			{children}
