da46f18ff3efbf4d652a6ae798395c8e69f26d8c
pongzers <you@example.com>
2021-11-04 07:24:54 +0800

    update sidebar

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -70,10 +70,18 @@ const Sidebar: React.FC<Props> = (props) => {
 	const className = `${isOpen ? styles.sidebar : styles.closeSidebar}`;
 	return (
 		<div className={className}>
+			{/* Implementing custom overlay to close sidebar on screen click
+			Manual override is needed because the library is buggy */}
+			<div 
+				onClick={() => handleClose()} 
+				className={styles.backdrop}
+				onKeyDown={() => handleClose()}
+			/>
 			<MultilevelSidebar
 				open={isOpen}
 				options={options}
-				// onItemClick={handleClose}
+				onItemClick={handleClose}
+				style={{zIndex: '105 !important'}}
 			/>
 			{/* using in our button to open the sidebar */}
 			<div
