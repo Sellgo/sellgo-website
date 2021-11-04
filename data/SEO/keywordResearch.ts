fd2440cc3380188cf2dcba80e30ff899df7feef7
rwprasojo <richard@sellgo.com>
2021-11-04 13:51:38 -0700

    content/BUGFR-2554

--- data/SEO/keywordResearch.ts
+++ data/SEO/keywordResearch.ts
@@ -2,8 +2,21 @@ import { SEODetails } from '../../interfaces/SEO';
 
 export const seoData: SEODetails = {
 	title: `Sellgo - Amazon Keyword Research: Amazon All-In-One Tool`,
-	description: ``,
+	description: `Powerful browser extension and Amazon Keyword Research tool. 
+	Evalute thousands of relevant keywords, analyze competitions, and identify high volume searches, 
+	seasonal keywords to grow your Amazon business. 
+	Amplify your Amazon product listing conversion with reliable keyword research tools.`,
 	imageUrl: ``,
-	keywords: [''],
+	keywords: [
+		'sellgo',
+		'sellgo chrome extension',
+		'sellgo extension',
+		'helium10',
+		'amazon keyword',
+		'amazon keyword finder',
+		'amazon keyword research',
+		'amazon keyword database',
+		'amazon keyword tracker'
+	],
 	slug: '/keyword-research'
 };
