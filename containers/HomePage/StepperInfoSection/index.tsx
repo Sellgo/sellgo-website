cf9fea8ae6ed775ed22f085d2aeb51fad7aa6b8f
pongzers <you@example.com>
2021-11-05 07:29:53 +0800

    Stepper content update

--- containers/HomePage/StepperInfoSection/index.tsx
+++ containers/HomePage/StepperInfoSection/index.tsx
@@ -1,12 +1,8 @@
-import React, { useState } from 'react';
-import { Tabs, TabList, Tab, TabPanel, resetIdCounter } from 'react-tabs';
-import { v4 as uuid } from 'uuid';
+import React from 'react';
 
 /* Data */
 import {
 	wholesaleFeatures,
-	privateLabelFeatures,
-	serviceProviderFeatures
 } from './data';
 
 /* Components */
@@ -18,44 +14,13 @@ import styles from './index.module.scss';
 interface Props {}
 
 const StepperInfoSection: React.FC<Props> = () => {
-	const [selectedPlanType, setSelectedPlanType] = useState<number>(0);
-	resetIdCounter();
-
 	return (
 		<section
 			className={`page-container ${styles.stepperInfoSectionWrapper}`}
 			id="showCtaNavBar"
 		>
-			<h2 className={styles.stepperTitle}>How Sellgo Works For</h2>
-			<Tabs
-				selectedTabClassName={styles.stepperTab__selected}
-				onSelect={setSelectedPlanType}
-				selectedIndex={selectedPlanType}
-			>
-				<TabList className={styles.tabListWrapper}>
-					<Tab key={uuid()} className={styles.stepperTab}>
-						WHOLESALE
-						<div className={styles.underline} />
-					</Tab>
-					<Tab key={uuid()} className={styles.stepperTab}>
-						PRIVATE LABEL
-						<div className={styles.underline} />
-					</Tab>
-					<Tab key={uuid()} className={styles.stepperTab}>
-						SERVICE PROVIDER
-						<div className={styles.underline} />
-					</Tab>
-				</TabList>
-				<TabPanel key={uuid()}>
-					<Stepper steps={wholesaleFeatures} />
-				</TabPanel>
-				<TabPanel key={uuid()}>
-					<Stepper steps={privateLabelFeatures} />
-				</TabPanel>
-				<TabPanel key={uuid()}>
-					<Stepper steps={serviceProviderFeatures} />
-				</TabPanel>
-			</Tabs>
+			<h2 className={styles.stepperTitle}>How Sellgo Works</h2>
+			<Stepper steps={wholesaleFeatures} />
 		</section>
 	);
 };
