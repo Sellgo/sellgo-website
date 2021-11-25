61e6cd841c1bda6b4e1bdced49a2cdaf7dc8d88e
pongzers <you@example.com>
2021-11-26 05:05:21 +0800

    Redirect to calendly link

---
+++ pages/onboarding/index.tsx
@@ -0,0 +1,3 @@
+import redirect from 'nextjs-redirect';
+// eslint-disable-next-line max-len
+export default redirect(`https://calendly.com/sellgo-richard/30-min-onboarding?utm_source=support&utm_medium=email&utm_campaign=onboarding`)
