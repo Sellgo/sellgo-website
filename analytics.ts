87dbe51ffa367c29f9badba3fb283507955e4b87
pongzers <you@example.com>
2022-04-18 09:58:12 -0700

    Add google tag manager

--- analytics.ts
+++ analytics.ts
@@ -3,13 +3,19 @@ import Analytics from 'analytics';
 // @ts-ignore
 import fullStoryPlugin from '@analytics/fullstory';
 
+// @ts-ignore
+import googleTagManager from '@analytics/google-tag-manager';
+
 const analytics = Analytics({
 	debug: process.env.NODE_ENV === 'development',
 	app: 'Sellgo Website',
 	plugins: [
 		fullStoryPlugin({
 			org: String(process.env.NEXT_PUBLIC_FULL_STORY_ID)
-		})
+		}),
+		googleTagManager({
+			containerId: String(process.env.NEXT_PUBLIC_GTM_ID)
+		}),
 	]
 });
 
