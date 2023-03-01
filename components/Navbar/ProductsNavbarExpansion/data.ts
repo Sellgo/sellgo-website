a89e0aba1ae5a2bcb24124922dc831a437be14d9
Richard <richard@sellgo.com>
2023-02-28 17:14:30 -0800

    update payment

--- components/Navbar/ProductsNavbarExpansion/data.ts
+++ components/Navbar/ProductsNavbarExpansion/data.ts
@@ -20,45 +20,6 @@ export const productsNavigationList: ProductsNavigationList[] = [
 ];
 
 export const productsRightPanelData: ProductsPanelData[] = [
-	/* {
-		hoverId: 'privateLabel',
-		header: '',
-		groupedNavLinksData: [
-			{
-				head: 'Extension',
-				desc: 'Product Validation on Amazon Page',
-				navigateTo: '/extension',
-				iconName: 'extension'
-			},
-			{
-				head: 'Keyword Finder',
-				desc: 'Reveal competitor keywords',
-				navigateTo: '/keyword-finder',
-				iconName: 'keywordFinder',
-				isNew: true
-			},
-			{
-				head: 'Keyword Database',
-				desc: 'Check high-volume keywords',
-				navigateTo: '/keyword-database',
-				iconName: 'keywordDatabase',
-				isNew: true
-			},
-			{
-				head: 'Product Rank Tracker',
-				desc: 'Get to #1 search results',
-				navigateTo: '/product-rank-tracker',
-				iconName: 'productRankTracker',
-				isNew: true
-			},
-			{
-				head: 'Product Database',
-				desc: 'Amazon product catalogue',
-				navigateTo: '/product-database',
-				iconName: 'productDatabase'
-			}
-		]
-	}, */
 	{
 		hoverId: 'serviceProvider',
 		header: '',
